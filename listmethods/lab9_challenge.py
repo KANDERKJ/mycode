d8b3fd7911cbcafecc366ae3323a26d1464ae03c
KANDERKJ <kanderk21@gmail.com>
2020-07-27 17:17:51 -0400

    add list directories

---
+++ listmethods/lab9_challenge.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+
+proto3 = [40, 60, 50, 30]
+
+print(proto3)
+
+list.reverse(proto3)  # pass proto3 through the reverse method
+print(proto3)          # will print proto3 in reverse order
+
+list.sort(proto3)   # pass proto3 through the sort method
+print(proto3)       # will print proto3 in sorted order from least to greatest
+
+
