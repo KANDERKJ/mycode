26da9345509ace126795f416334b17193da7cea2
KANDERKJ <kanderk21@gmail.com>
2020-07-27 18:01:19 -0400

    add Chad challenge 2

---
+++ challenge_lab02.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+col_list = ["blue", "Columbus"]
+
+name= input("What is your name? ")
+
+col_list.append(1492)
+print(f"In 1492,  {col_list[1]} sailed the ocean {col_list[0]}. {name} fell of the boat. ")
