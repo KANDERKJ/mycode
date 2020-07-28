2a9bbb6a4f11ea45f134b8214a0a241d189b0afd
KANDERKJ <kanderk21@gmail.com>
2020-07-28 12:57:18 -0400

    add dictionary exercise

---
+++ challenge_lab01.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+
+name = input('What is your name?: ')
+age = input('How old are you?: ')
+height = input('How tall are you?: ')
+
+print("Hello, " + name + "! " + "You're " + age + "? Super old! " + "You're " + height + "? Super tall! ")
+
+print(f"Hello, {name}! You're age {age} super old! You're height? {height} You're super tall! ")
+
+
