cbe48a582eeb52d89065798d641e4a59b0f5ae60
KANDERKJ <kanderk21@gmail.com>
2020-07-28 17:20:55 -0400

    if, elif, else prac

--- moveplease01.py
+++ moveplease01.py
@@ -3,12 +3,12 @@
 import shutil
 import os
 
-os.chdir('/home/student/mycode/')
+os.chdir('/home/student/mycode/')   # naming path to execute the follwing script
 
 shutil.move('raynor.obj', 'ceph_storage/')  # we created file in /mycode this will move file to /ceph_storage
 
-xname = input('What is the new name for kerrigan.obj? ')    # renameing the file with user input
+xname = input('What is the new name for kerrigan.obj? ')    # saving user input as a variable
 
-shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname) # move the newly named file to ceph_storage
+shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname) # using concantenation to add the user input and complete the path 
 
 
