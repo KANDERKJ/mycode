78f45f336c79c5f20ea697df53611f31dd63cb96
KANDERKJ <kanderk21@gmail.com>
2020-07-29 14:38:31 -0400

    for, range(), and with practice

---
+++ introfor/forloop3custom.py
@@ -0,0 +1,4 @@
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+# CODE CUSTOMIZATION 01 - Create a for-loop that displays the following data structure. How the data is displayed is up to you!
---
+++ introfor/forloop_chal6.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# write a script that constructs the following pattern using a for loop.
+
+
+# Example: ["*", "**",.... etc.]
+# take each 'line' of this triangle and put it in  a list
+
+stars = ['*', '* *', '* * *', '* * * *', '* * * * *']
+reverse = ['* * * *', '* * *', '* *', '*']
+
+for x in stars:
+    print(x)    
+
+for x in reverse:
+    print(x)
+
+
+    
