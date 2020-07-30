4da917ae459482466932cdc8b3dc74acdf2c93a0
KANDERKJ <kanderk21@gmail.com>
2020-07-30 13:53:56 -0400

    Learning to parse log files

--- introfor/forloop3custom.py
+++ introfor/forloop3custom.py
@@ -2,3 +2,38 @@ farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens"
          {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
          {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
 # CODE CUSTOMIZATION 01 - Create a for-loop that displays the following data structure. How the data is displayed is up to you!
+
+
+# EASY(ish):
+#• Write a for loop that returns all the animals from the NE Farm!
+
+# MEDIUM:
+#• Ask a user to choose a farm (NE Farm, W Farm, or SE Farm). Return the plants/animals that are raised on that farm.
+
+# HARD:
+#• Ask a user to choose a farm (NE Farm, W Farm, or SE Farm). Return the plants/animals that are raised on that farm.
+
+#• ...but only return ANIMALS from that particular farm.
+
+
+print("Here at the NE Farm, we have the following animals:")
+for every_animal in farms[0]["agriculture"]:
+    print(every_animal) # print(every_animal, end=" ") will print horizontally
+
+# simplest thing would be three different for loops (one hard coded to each farm) which are selected by if/elif
+dude = input()
+for dude in farms[0]["agriculture"]:
+    if dude in
+    print()
+    elif due not in
+
+for dude in farms[1]["agriculture"]:
+    if dude in
+    print()
+    elif dude in
+
+for dude in farms[2]["agriculture"]:
+    if dude in
+    print()
+    else:
+
--- introfor/looping02d.py
+++ introfor/looping02d.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
+# dnsservers.txt = original file with lines contain .org's and .com's
+
 # open file in read mode
-with open("dnsservers.txt", "r") as dnsfile:
+with open("dnsservers.txt", "r") as dnsfile: #open the exisiting file of servers(dnsserveers.txt) and reading    them off into the dnsfile variable
     # indent to keep the dnsfile object open
     # loop across the lines in the file
     for svr in dnsfile:
-        svr = svr.rstrip('\n') # remove newline char if exists
+        svr = svr.rstrip('\n') # remove newline char if exists 
                                # would exists on all but last line
         # IF the string svr ends with 'org'
         if svr.endswith('org'):
-            with open("org-domain.txt", "a") as srvfile:
+            with open("org-domain.txt", "a") as srvfile: # open org-domain.txt and append the lines of srvfile
                 srvfile.write(svr + "\n")
         # ELSE-IF the string svr ends with 'com'
         elif svr.endswith('com'):
@@ -17,3 +19,11 @@ with open("dnsservers.txt", "r") as dnsfile:
                 srvfile.write(svr + "\n")
 # no need to close our file - closed automatically
 
+
+
+# 1st if takes the variable svrfile and writes the lines in svr into the file org-domain.txt
+# 2nd elif takes the variable svrfile and writes the lines in svr into the file com-domain.txt
+
+# data changes all the time! data is typically not stored inside python scripts.
+# python reads fastest through dictionaries, most data is stored in spreadsheet
+# spreadsheet is actually just a dictionary
