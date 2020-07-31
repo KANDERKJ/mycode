85684032cc2822f5852b6c32bba87a2042cb677d
KANDERKJ <kanderk21@gmail.com>
2020-07-31 11:05:57 -0400

    calculator challenge

---
+++ netfunct01/calculator.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python3
+
+
+def add(cash, money):
+    return  cash + money
+
+def sub(cash, money):
+    return cash - money
+
+def mult(cash, money):
+    return cash * money
+
+def div(cash, money):
+    return cash / money
+
+print("Select below:\n" \
+      "1. Add\n" \
+      "2. Subtract\n" \
+      "3. Multiply\n" \
+      "4. Divide\n")
+
+int1= float(input("Choose a number: "))
+int2= float(input("Choose a second number: "))
+
+message = int(input("What would you like to do? 1, 2, 3, ,4 :"))
+
+if message == 1:
+    print(int1 + int2)
+elif message == 2:
+    print(int1 - int2)
+elif message == 3:
+    print(int1 * int2)
+elif message == 4:
+    print(int1 / int2)
+else:
+    print("Invalid")
+
+    
